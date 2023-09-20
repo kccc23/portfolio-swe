05034736c9867a0cb5c6ab59e6347f3a4928a0df
Kaining <kainingchen2013@gmail.com>
2023-09-19 23:32:25 -0400

    playing around skills

--- src/components/SkillsC.tsx
+++ src/components/SkillsC.tsx
@@ -14,16 +14,36 @@ const SkillsC: React.FC = () => {
         };
     }, []);
 
+    const textColors = [
+        'text-red-500', 'text-yellow-500', 'text-green-500', 'text-grey-500',
+        'text-indigo-500', 'text-purple-500', 'text-pink-500'
+    ];
+
+    const randomTextColor = () => {
+        const randomIndex = Math.floor(Math.random() * textColors.length);
+        return textColors[randomIndex];
+    };
+
     return (
         <div className="flex flex-wrap justify-center items-center h-screen">
             {data.skills.map((skill, index) => {
+                const baseSize = 0.5; // 0.5rem
                 const zoom = 1 + scrollY * 0.005;
                 const moveDistance = index % 2 === 0 ? scrollY * 0.5 : scrollY * -0.5;
+                const fontSize = `${baseSize + index * 0.25}rem`;
+                const gridColumn = 1 + (index % 4);
+                const gridRow = 1 + Math.floor(Math.random() * 4);
+                const textColor = randomTextColor();
                 return (
                     <div
                         key={skill.id}
-                        style={{ transform: `translateX(${moveDistance}px) scale(${zoom})` }}
-                        className="m-4 p-2 flex items-center justify-center rounded shadow-md text-lg transform transition-transform duration-300"
+                        style={{
+                            transform: `translateX(${moveDistance}px) scale(${zoom})`,
+                            fontSize: fontSize,
+                            gridColumn: gridColumn,
+                            gridRow: gridRow
+                        }}
+                        className={`m-4 transform transition-transform duration-300 ${textColor}`}
                     >
                         {skill.name}
                     </div>
